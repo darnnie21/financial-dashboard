b90a497586fd8d16f168bef2913d7bc9a864daf3
Ubah Chukwuemeka <danielchuks082@gmail.com>
2023-11-30 14:12:01 +0100

    added user dashboard layout

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Invoices = (props: Props) => {
+  return (
+    <div>Invoices Page</div>
+  )
+}
+
+export default Invoices
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <p>Dashboard Page</p>
+  )
+}
+
+export default Dashboard
